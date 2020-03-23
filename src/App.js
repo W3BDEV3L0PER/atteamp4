707d37a522b60db8037e4d9aee743a031143132f
alex228 <sashhha1234321@gmail.com>
2020-03-23 12:00:32 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isButtonClicked:false
+  }
+
+  render(){
+      return (
+          <div className="App">
+           <header className="App-header">
+            <div className={ this.state.isButtonClicked ? "on" : "off" }
+              onClick={() => this.setState({isButtonClicked: !this.state.isButtonClicked})}>
+                  { this.state.isButtonClicked ? 'on' : 'off' }
+            </div>
+            <div id="card" onMouseMove={function location(e){
+                let elem = document.getElementById('card')
+                let X = e.nativeEvent.offsetX
+                let Y = e.nativeEvent.offsetY
+                let width = elem.offsetWidth
+                let height = elem.offsetHeight
+                elem.style.transform = "rotateX(" + -(Y  - width / 2) / 10 + "deg) rotateY(" + (X - height / 2) / 10 + "deg)"
+            }} onMouseOut={function relocation(e){
+                let elem = document.getElementById('card')
+                elem.style.transform = "rotateX(0) rotateY(0)"
+            }}>
+            </div>
+           </header>
+          </div>
+      );
+    }
 }
 
 export default App;
